e827b7f7772ce460a834792164b7ee79275e93f7
CapacitorSet <capacitorset@gmail.com>
2020-12-12 02:52:54 +0100

    day9: cleanup

--- day9/day9.main.kts
+++ day9/day9.main.kts
@@ -7,38 +7,35 @@ var lines = File("input").readLines().map { it.toLong() }
 val buffer = lines.take(25)
 val numbers = lines.drop(25)
 
+fun List<Long>.anyIndexed(f: (index: Int, item: Long) -> Boolean): Boolean {
+    return this.filterIndexed(f).isNotEmpty()
+}
+
 val target = numbers.fold(State(buffer)) { state, num ->
-    val isCorrect = state.buffer.filterIndexed { idx_a, a ->
-        state.buffer.filterIndexed { idx_b, b -> idx_a != idx_b && (a + b == num) }.isNotEmpty()
-    }.isNotEmpty()
-    val newTarget = if (!isCorrect && state.target == -1L) num else state.target
-    // "ring" buffer: drop the last elt and push a new one
-    val newBuffer = state.buffer.drop(1) + num
-    if (!isCorrect) {
-        print("$num is not correct. Possible choices: ")
-        println(state.buffer.flatMap { a -> state.buffer.map { b -> a + b } }.toSortedSet())
+    val isCorrect = state.buffer.anyIndexed { idx_a, a ->
+        state.buffer.anyIndexed { idx_b, b -> idx_a != idx_b && (a + b == num) }
     }
-    State(newBuffer, newTarget)
+    State(
+        target = if (!isCorrect && state.target == -1L) num else state.target,
+        // "ring" buffer: drop the last elt and push a new one
+        buffer = state.buffer.drop(1) + num
+    )
 }.target
 
 println("Part one: $target")
 
-var answer = 0L
+for_loop@
+// Iterate until the cumulative sum meets or exceeds target
 for (begin in lines.indices) {
-    var sum = 0L
-    // Iterate until the cumulative sum meets or exceeds target
-    for (end in lines.indices.drop(begin)) {
+    for (end in begin until lines.size) {
         val interval = lines.subList(begin, end)
-        sum = interval.sum()
-        if (sum >= target) {
-            // "Speculatively" compute the answer. Not super elegant but eh variable scoping sucks
-            answer = interval.min()!! + interval.max()!!
-            break
+        val sum = interval.sum()
+        when {
+            (sum > target) -> break
+            (sum == target) -> {
+                println("Part two: " + (interval.min()!! + interval.max()!!))
+                break@for_loop
+            }
         }
     }
-    if (sum == target) {
-        break
-    }
 }
-
-println("Part two: $answer")
