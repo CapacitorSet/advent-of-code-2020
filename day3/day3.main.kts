27936e6dd341d4a78005c6f8efa683c954198b12
CapacitorSet <capacitorset@gmail.com>
2020-12-12 02:05:53 +0100

    day3: cleanup

--- day3/day3.main.kts
+++ day3/day3.main.kts
@@ -10,17 +10,15 @@ fun treeCountReducer(slope: Int): (State, String) -> State =
             if (line[index] == '#') state.treeCount + 1 else state.treeCount)
     }
 
-fun treeCount(lines: Array<String>, slope: Int): Int =
+fun treeCount(lines: List<String>, slope: Int): Int =
     lines.fold(State(), treeCountReducer(slope)).treeCount
 
-val lines = File("input").readLines().toTypedArray()
+val lines = File("input").readLines()
 
 val slope1 = treeCount(lines, 1)
 val slope3 = treeCount(lines, 3)
 val slope5 = treeCount(lines, 5)
 val slope7 = treeCount(lines, 7)
-val slope1d1 = treeCount(lines
-    .filterIndexed { idx, _ -> idx.rem(2) == 0 }
-    .toTypedArray(), 1)
+val slope1d1 = treeCount(lines.filterIndexed { idx, _ -> idx.rem(2) == 0 }, 1)
 println("Part one: $slope3")
 println("Part two: " + (slope1.toLong()*slope3.toLong()*slope5.toLong()*slope7.toLong()*slope1d1.toLong()))
