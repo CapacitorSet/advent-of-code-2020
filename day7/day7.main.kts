31af606aacb3e2ff6452e562cb91865214b30749
CapacitorSet <capacitorset@gmail.com>
2020-12-12 02:31:38 +0100

    day7: cleanup

--- day7/day7.main.kts
+++ day7/day7.main.kts
@@ -6,20 +6,17 @@ fun parseBags(str: String): Pair<String, Int> {
     return Pair(parts[1] + " " + parts[2], parts[0].toInt())
 }
 
-// Does the haystackBag contain needle eventually?
-// (This could be memoized but the input is small enough that this isn't worth the additional complexity.)
-fun bagContains(needle: String, haystackBag: String): Boolean {
-    return haystackBag == needle
-        || bags[haystackBag]!!.keys.any { bagContains(needle, it) }
-}
-
 // Apply the function f to the bag src and its descendants, with multiplicity
 // (i.e. two bags of the same colour will result in applying f twice)
-fun<T> bagWalk(src: String, f: (String) -> T) {
-    f(src)
+fun<T> bagReduce(initialValue: T, withMultiplicity: Boolean, src: String, f: (T, String) -> T): T {
+    var ret = f(initialValue, src)
     bags[src]?.forEach { bag, count ->
-        repeat(count) { bagWalk(bag, f) }
+        if (withMultiplicity)
+            repeat(count) { ret = bagReduce(ret, withMultiplicity, bag, f) }
+        else
+            ret = bagReduce(ret, withMultiplicity, bag, f)
     }
+    return ret
 }
 
 val lines = File("input").readLines()
@@ -33,8 +30,10 @@ val bags: Map<String, Map<String, Int>> = lines.map {
     Pair(colour, children)
 }.toMap()
 
+fun bagContainsShinyGold(bag: String): Boolean =
+    bagReduce(false, withMultiplicity = false, src = bag)
+        {hasShinyGold, bag -> hasShinyGold || bag == "shiny gold" }
+
 // Minus one because the shiny gold bag itself matches
-println("Part one: " + (bags.keys.count { bagContains("shiny gold", it) } - 1))
-var bagCounter = 0
-bagWalk("shiny gold") { bagCounter++ }
-println("Part two: " + (bagCounter - 1))
+println("Part one: " + (bags.keys.count { bagContainsShinyGold(it) } - 1))
+println("Part two: " + bagReduce(-1, withMultiplicity = true, "shiny gold") { counter, _ -> counter + 1 })
